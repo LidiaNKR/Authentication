7e6d8fa624a8f85ea75fff26e193e6b75f7dee6e
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2024-04-29 11:04:38 +0300

    Initial Commit

---
+++ Authentication/AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationApp.swift
+//  Authentication
+//
+//  Created by Лидия Некрасова on 29.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
